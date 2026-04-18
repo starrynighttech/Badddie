ad7398e11a055c92bb103736f86009d18e34ef6c
starrynighttech <starrynight8531@gmail.com>
2026-04-18 19:25:19 +0800

    Create ads.js

---
+++ backend/routes/ads.js
@@ -0,0 +1,17 @@
+const router = require("express").Router()
+const Ad = require("../models/Ad")
+const { addMoney } = require("../services/walletService")
+
+router.get("/", async (req, res) => {
+  res.json(await Ad.find())
+})
+
+router.post("/reward", async (req, res) => {
+  const { userId, reward } = req.body
+
+  await addMoney(userId, reward)
+
+  res.json({ success: true })
+})
+
+module.exports = router
