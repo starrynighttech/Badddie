20999d9556c70c0975e533eda04ac5b3cd3606b8
starrynighttech <starrynight8531@gmail.com>
2026-04-18 19:40:12 +0800

    Create roles.js

---
+++ backend/middleware/roles.js
@@ -0,0 +1,11 @@
+module.exports = (...allowedRoles) => {
+  return (req, res, next) => {
+    const user = req.user
+
+    if (!user || !allowedRoles.includes(user.role)) {
+      return res.status(403).json({ error: "Access denied" })
+    }
+
+    next()
+  }
+}
