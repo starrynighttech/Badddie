cc267f619c387953b4e5e415f9a5a3f837801570
starrynighttech <starrynight8531@gmail.com>
2026-04-18 18:39:41 +0800

    Create products.js

---
+++ backend/routes/products.js
@@ -0,0 +1,15 @@
+const router = require("express").Router()
+const Product = require("../models/Product")
+
+router.get("/", async (req, res) => {
+  const products = await Product.find()
+  res.json(products)
+})
+
+router.post("/", async (req, res) => {
+  const product = new Product(req.body)
+  await product.save()
+  res.json(product)
+})
+
+module.exports = router
